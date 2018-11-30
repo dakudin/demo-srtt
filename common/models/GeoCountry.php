6057515b3477795b8c2311006ff04bf553467129
dakudin <dakudin@gmail.com>
2018-11-30 09:43:58 +0200

    Change country suggesting

--- common/models/GeoCountry.php
+++ common/models/GeoCountry.php
@@ -54,7 +54,7 @@ class GeoCountry extends \yii\db\ActiveRecord
     public static function getCountriesByName($name = null, $limit = 10)
     {
         return Yii::$app->db->createCommand('SELECT country_name FROM geo_country WHERE country_name LIKE :name ORDER BY country_name LIMIT :limit')
-            ->bindValue(':name', $name.'%')
+            ->bindValue(':name', '%'.$name.'%')
             ->bindValue(':limit', $limit)
             ->queryAll();
     }
