287c04e2a79fd5d2ada1c581ee92c79c1b57c47d
dakudin <dakudin@gmail.com>
2019-02-28 17:01:54 +0200

    small fix

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css?v=0.14',
+        'css/site.css?v=0.15',
     ];
     public $js = [
     ];
