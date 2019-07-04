89acbc166f1fcdd302d6042e5832a489d84058c8
dimitry <dakudin@gmail.com>
2019-07-04 09:54:31 +0300

    Enable sending real enquiries

--- common/config/params.php
+++ common/config/params.php
@@ -4,7 +4,7 @@ return [
     'supportEmail' => 'support@sortit.com',
     'dummySupportEmail' => 'support@demosortit.com',
     'user.passwordResetTokenExpire' => 3600,
-    'sendRealQuote' => false, // set to true for sending quotes to real retailers
+    'sendRealQuote' => true, // set to true for sending quotes to real retailers
 
     'enquiryEmailSendFrom' => 'enquiry@demosortit.com',
     'enquiryEmailSendTo' => 'charlie.hollinrake@gmail.com',
