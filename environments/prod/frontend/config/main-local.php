c2cda4b9583b6d9e1f35f38f935d98b222c00c59
dakudin <dakudin@gmail.com>
2019-11-19 16:14:35 +0200

    Small fix

--- environments/prod/frontend/config/main-local.php
+++ environments/prod/frontend/config/main-local.php
@@ -10,8 +10,8 @@ return [
             'clients' => [
                 'facebook' => [
                     'class' => 'yii\authclient\clients\Facebook',
-                    'clientId' => '2076627205736800', //'482427341961320',
-                    'clientSecret' => '61db32969df60fc6b8e630e8eedf5e41', //'39e15e876dfb129d51163a02909c9416',
+                    'clientId' => '',
+                    'clientSecret' => '',
                     'authUrl' => 'https://www.facebook.com/v3.1/dialog/oauth',
                     'attributeNames' => ['email', 'name', 'first_name', 'last_name',
                         //'user_age_range', 'user_birthday',  'user_gender', 'user_hometown', 'user_location'
@@ -20,8 +20,8 @@ return [
                 ],
 //                'amazon' => [
 //                'class' => 'common\components\AmazonAuthClient',
-//                'clientId' => 'amzn1.application-oa2-client.a0e54471f93d4381a217ad024d3f3e5a',
-//                'clientSecret' => 'ec1df148e5f62a3e4a349ed653043ced443fcb18ef282d976f9f8214697576c5',
+//                'clientId' => '',
+//                'clientSecret' => '',
 //                ],
             ],
         ],*/
