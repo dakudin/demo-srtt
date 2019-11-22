24cc8e9c8e61c1b0dcd8411e217425890c233bd9
dakudin <dakudin@gmail.com>
2019-11-22 10:29:32 +0200

    Social buttons added

--- frontend/views/layouts/_footer.php
+++ frontend/views/layouts/_footer.php
@@ -1,8 +1,17 @@
     <footer class="footer">
         <div class="container">
-            <p class="pull-left">&copy; Sortit <?= date('Y') ?></p>
-            <p class="pull-left"><a class="padding-base-horizontal" href="<?= $this->params['footer_links']['termsofuse_url'] ?>" target="termsOfUse">Terms of Use</a></p>
-            <p class="pull-left"><a href="<?= $this->params['footer_links']['privacy_police_url'] ?>" target="privacyAndCookies">Privacy Policy</a></p>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="pull-left">&copy; Sortit <?= date('Y') ?></div>
+                    <div class="pull-left"><a class="padding-base-horizontal" href="<?= $this->params['footer_links']['termsofuse_url'] ?>" target="termsOfUse">Terms of Use</a></div>
+                    <div class="pull-left"><a href="<?= $this->params['footer_links']['privacy_police_url'] ?>" target="privacyAndCookies">Privacy Policy</a></div>
+
+                    <div class="social-icons pull-right">
+                        <a href="https://www.facebook.com/wesortitforyou/" target="_blank"><img src="images/facebook.png" alt="We are on Facebook"></a>
+                        <a href="https://www.linkedin.com/company/sortit.com/about/" target="_blank"><img src="images/linkedin.png" alt="We are on LinkedIn"></a>
+                    </div>
+                </div>
+            </div>
         </div>
     </footer>
 
